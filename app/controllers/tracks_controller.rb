6c6ad95c49efc875e81d418260bc57e20433af4d
MiBr7 <michi_brandt@t-online.de>
2017-03-22 13:34:26 +0100

    Add track search and Preview function

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,4 +1,9 @@
 class TracksController < ApplicationController
   def index
+    if !params[:tracks_name].empty?
+      @tracks = RSpotify::Track.search(params[:tracks_name])
+    else
+      redirect_to root_path
+    end
   end
 end
